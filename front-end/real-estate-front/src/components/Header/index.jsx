2d5220f849245292b7756e95d2a1e596ea7d5cc0
Kelvin <kelvinnicolau7@gmail.com>
2023-11-09 21:18:31 -0300

    feat: instalando styled components e start project

---
+++ front-end/real-estate-front/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+function Header () {
+    return (
+      <>
+        <div>
+          <h2>
+            Hello World!
+          </h2>
+        </div>
+      </>
+    )
+  }
+  
+  export default Header
+  
