637464d38e9c57a87126360910d1a938484bb504
Kelvin <kelvinnicolau7@gmail.com>
2023-11-15 23:14:52 -0300

    feat: criando banco de dados com prisma

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -1,7 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 export default {
     async createUser(request, response){
+
+        const { name, email, password } = request.body
+
         try {
-            
+            const user = await prisma.user.findUnique({ where: { email} });
+
+            if(!user){
+                return response.json({ message: "Usuário não existe"});
+            }
+
+            user = await prisma.user({
+                data: {
+                    name,
+                    email,
+                    password
+                }
+            });
+
+            return response.json(user);
+
         } catch (error) {
             return response.json({ message: error.message })
         }
